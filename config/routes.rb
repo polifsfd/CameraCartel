ec735edb17e7c31de1e838625f023871b035a30c
Pedro Duarte <pedromsd21@gmail.com>
2024-01-31 12:32:00 +0100

    route update

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :cameras
-  resources :bookings do
-    member do
-      put :approve
-      put :deny
-    end
-  end
+  resources :bookings
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
