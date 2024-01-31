0169f6170e250950a148a30ee857feeace56e71b
Pedro Duarte <pedromsd21@gmail.com>
2024-01-31 12:24:20 +0100

    added status and changed view/controller

--- db/migrate/20240131105452_create_bookings.rb
+++
@@ -1,8 +0,0 @@
-class CreateBookings < ActiveRecord::Migration[7.1]
-  def change
-    create_table :bookings do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_31_105452) do
-
+ActiveRecord::Schema[7.1].define(version: 2024_01_31_112115) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_31_105452) do
     t.bigint "camera_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "status"
     t.index ["camera_id"], name: "index_bookings_on_camera_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
