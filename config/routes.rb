107fa9e6133fda4d66ef238eee6824bbe390f475
Arnaud Lahaut <arnaudlht@gmail.com>
2024-02-01 22:30:05 +0100

    Replaced category tag by actual categories names

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     resources :bookings, only: [:create, :new]
   end
   resources :bookings, only: [:edit, :update, :index]
-  resources :categories, only: [:index]
+  resources :categories, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
