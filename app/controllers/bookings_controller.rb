dff2ae2b22a5dbac07b2f737e30e97ff7e615f2c
Arnaud Lahaut <arnaudlht@gmail.com>
2024-02-01 23:07:45 +0100

    dashboard backend done for today

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,7 +6,6 @@ class BookingsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    #@bookings = Booking.all
     @bookings = current_user.bookings
     @bookings_as_owner = current_user.bookings_as_owner
   end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,8 @@ class DashboardController < ApplicationController
 
   def index
     @user = current_user
+    @bookings = current_user.bookings
+    @bookings_as_owner = current_user.bookings_as_owner
+    @cameras = current_user.cameras
   end
 end
