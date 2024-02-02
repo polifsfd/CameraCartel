5eb23ba1abd6e1c854e26a3bdea4b11ba9d7432d
Pedro Duarte <pedromsd21@gmail.com>
2024-02-02 10:31:03 +0100

    cameras individual front done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_01_101917) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_01_203827) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_01_101917) do
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
